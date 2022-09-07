d76a8b38a7c49b168e5b8e64b4080b781a08ae9d
aleksandra <akkovaleva14@gmail.com>
2022-09-07 10:59:03 +0300

    Now two screens, some packages. Little updates on functionality

--- app/src/main/java/com/application/yourmap/ui/fragments/LaunchFragment.kt
+++ app/src/main/java/com/application/yourmap/ui/fragments/LaunchFragment.kt
@@ -1,12 +1,8 @@
 package com.application.yourmap.ui.fragments
 
 import android.Manifest
-import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.location.LocationManager
 import android.os.Bundle
-import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
